9042a637cd9c50ac49397a3bf5d0eca7cd37a074
thesocalledhonouredone <katuruanirudh1@gmail.com>
2026-02-08 22:05:14 +0530

    updated GET getProfile end point and added status codes to the auth middlewares

--- backend/controllers/profile.controller.js
+++ backend/controllers/profile.controller.js
@@ -3,14 +3,21 @@ import Post from "../models/Post.js";
 import Likes from "../models/Likes.js";
 
 async function getProfile(req, res) {
-  const userId = req.user.id
-  const userPosts = await Post.find({author: userId});
-  
-  if (!userPosts || userPosts.length < 1) {
-    return res.json({"message": "you havent posted anything"});
+  try {
+    const userId = req.user.id
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({message: "user not found"});
+    }
+
+    return res.status(200).json({
+      username: user.username,
+      bio: user.bio || null
+    });
+  } catch (err) {
+    console.log("Error: ", err);
+    return res.status(500).json({message: "server error"});
   }
-  
-  return res.json(userPosts);
 }
 
 async function putPfp(req, res) {
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,12 +3,12 @@ import { getUser } from "../services/auth.services.js";
 function checkLogin(req, res, next) {
   const token = req.cookies?.inkwell_auth_cookie;
   if (!token) {
-    return res.json({"message": "login first"});
+    return res.status(401).json({"message": "login first"});
   }
 
   const user = getUser(token);
   if (!user) {
-    return res.json({"message": "invalid token"});
+    return res.satus(401).json({"message": "invalid token"});
   }
 
   req.user = user;
